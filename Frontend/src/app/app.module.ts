e432559adfd6eb19aa33d12f29a1488198e4b219
Sushant <sushant.mhty@gmail.com>
2021-09-12 00:21:37 +0530

    Template Driven Forms in Angular

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
@@ -29,6 +30,7 @@ const appRoutes:Routes=[
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes)  
   ],
   providers: [
--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,8 +8,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-property.component.css']
 })
 export class AddPropertyComponent implements OnInit {
+  @ViewChild('Form') addPropertyForm:NgForm;
 
   constructor(private router:Router) { }
+  onSubmit(){
+    console.log('Congrats, form submitted');
+    console.log(this.addPropertyForm);
+  }
 
   ngOnInit() {
   }
